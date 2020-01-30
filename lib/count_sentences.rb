7ed2a0bea087e6072bd0d9af587ad8999db5287a
Abel Kalum <chumbabel@gmail.com>
2020-01-30 20:28:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -26,7 +26,7 @@ class String
     end
   end
 
-  def count_sentences(string)
+  def count_sentences(sentences)
     sentence_count = text.split(/\.|\?|!/).length
     puts "#{sentence_count} sentences"
      
